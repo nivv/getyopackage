f8130a5ced014cb99f7e00ef96a002aecc359c1d
Albin <albin.nilsson89@gmail.com>
2014-07-13 14:06:36 +0200

    updated one thang

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'host'      => '127.0.0.1',
+			'database'  => 'checkyopackage',
+			'username'  => 'checkyopackage',
+			'password'  => 'VJAfAL0XyjwcOBU08p5G',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
